c1a40a47e58cd06b2e7f9f6ecc38c650d00ab2f1
pngochop <44003101+pngochop@users.noreply.github.com>
2019-06-09 13:06:33 -0700

    Add files via upload
    new

--- main.py
+++ main.py
@@ -1,121 +1,89 @@
 """
-# File:     main.py
-# Author:   Zeeshan Karim
-# Date:     5/27/2017
-# Course:   TCSS 435 - Artificial Intelligence
-# Purpose:  Implementation of nGram English Language Generator
+# Author:   Hop N Pham
+# Version:     6/10/2019
+# Assigment:   Trigram	model	of	English
 """
 
-#import nGram
 import random
+STORY_LEN = 10 # len of the story to generates.
 
-# Depth of nGram structure
-N_GRAM = 5
+class Tri_Gram:
+    
+    # Initializes
+    def __init__(self, count, theTri_Sequence):
+        self.structor = []
+        self.appeared = 1
+        self.count = count
+        self.element = theTri_Sequence[0]
+        self.__insert(len(theTri_Sequence) > 1, theTri_Sequence)
 
-# Number of words to generate
-LEN_OUTPUT = 1000
-
-# Example file directory and filenames
-directory = 'ExampleText/'
-fileList = []
-fileList.append('alice-27.txt') 
-fileList.append('doyle-27.txt')
-fileList.append('doyle-case-27.txt')
-fileList.append('london-call-27.txt')
-fileList.append('melville-billy-27.txt')
-fileList.append('twain-adventures-27.txt')
-class nGram():
-    
-    # Initializes member variables and creates successor nGram objects
-    def __init__(self, level, params):
-        self.element = params[0]
-        self.count = 1
-        self.level = level
-        self.successors = []
-        if len(params) > 1:
-            self.successors.append(nGram(self.level + 1, params[1:]))
-    
-    # Recursive string representation of nGram model        
-    def __str__(self):
-        output = ''.join(['\t']*self.level) 
-        output += self.element + ' ' + str(self.count) + '\n'
-        for successor in self.successors:
-            output += str(successor)
-        return output
-    
-    # Recursively updates nGram model with a new word sequence
-    def update(self, params):
-        self.count += 1
-        exists = False
-        for successor in self.successors:
-            if params[0] == successor.element:
-                exists = True
-                successor.update(params[1:])
-                break
-        if not exists and len(params) > 0:
-            self.successors.append(nGram(self.level + 1, params))
-    
-    # Recursively generates weighted random word sequence        
     def getWords(self):
-        output = []
-        position = random.randint(0, self.count - 1)
-        for successor in self.successors:
-            position -= successor.count
+        story = []
+        position = random.randint(0, self.appeared - 1)
+        for successor in self.structor:
+            position -= successor.appeared
             if position < 0:
-                output.append(successor.element)
-                output += (successor.getWords())
+                story.append(successor.element)
+                story += (successor.getWords())
                 break
-        return output
-if __name__ == '__main__':
-    model = []
-    wordSequence = []
-    nWords = 0
+        return story
+		
+    def __insert(self,len,theTri_Sequence):
+        if (len) : self.structor.append(Tri_Gram(self.count + 1, theTri_Sequence[1:]))	
+		
+    def gramUpdate(self, theTri_Sequence):
+        self.appeared += 1
+        exists = False
+        for successor in self.structor:
+            if theTri_Sequence[0] == successor.element:
+                exists = True
+                successor.gramUpdate(theTri_Sequence[1:])
+                break
+        if not exists and len(theTri_Sequence) > 0:
+            self.structor.append(Tri_Gram(self.count + 1, theTri_Sequence))
     
-    # Iterate through files and add nGrams to model
-    for fileName in fileList:
-        print 'Reading ' + fileName + ' ...'
-        with open(directory + fileName, 'r') as openFile:
-            for line in openFile:
-                for word in line.split():
-                    nWords += 1
-                    wordSequence.append(word.lower())
-                    if len(wordSequence) == N_GRAM:
-                        exists = False
-                        for gram in model:
-                            if gram.element == wordSequence[0]:
-                                exists = True
-                                gram.update(wordSequence[1:])
-                                break
-                        if not exists:
-                            model.append(nGram.nGram(0, wordSequence))      
-                        wordSequence.remove(wordSequence[0])
 
-    '''
-    # Write string representation of model to model.txt
-    with open('model.txt', 'w') as outFile:
-        for gram in model:
-            outFile.write(str(gram) + '\n')
-    '''        
-    
-    # Select weighted random first word from model
-    output = []
-    position =  random.randint(0, nWords -1)
-    print 'Generating text ...'
-    for gram in model:
-        position -= gram.count
-        if position < 0:
-            output.append(gram.element)
-            break
-    
-    # Loop word selection based off first word till output length
-    while len(output) < LEN_OUTPUT:
-        for gram in model:
-            if gram.element == output[-1]:
-                output += gram.getWords()
+def loadWords(stories):
+    words = []
+    for story in stories:
+        with open(story, 'r') as lines:
+            for line in lines:
+                words.extend(line.lower().split())
+    return words   
+	
+def generateStory(theStructor):
+    story = []
+    story.append(random.choice(theStructor).element) # Randomly select first word.
+    # Generate the story base on the requirement length.
+    while len(story) < STORY_LEN:
+        for word in theStructor:
+            if word.element == story[-1]:
+                story += word.getWords()
                 break
-    
-    # Write generated text to output.txt
-    with open('output.txt', 'w') as outFile:
-        for word in output[0:LEN_OUTPUT - 1]:
-            outFile.write(word + ' ')
-    print 'Text written to output.txt'
+				
+    for i in range (STORY_LEN, len(story)):
+        story[i] = '';
+    with open('output.txt','w') as f:
+        f.write( ' '.join( story ) )
+    print 'Finished, please check the output file output.txt'
+	
+def updateModel(elem, target):	
+    if (elem.element == target[0]):
+        elem.gramUpdate(target[1:])
+        return True
+    return False
+	
+if __name__ == '__main__':
+    stories = ['alice-27.txt']#,'doyle-27.txt','doyle-case-27.txt','london-call-27.txt','melville-billy-27.txt','twain-adventures-27.txt']
+    tri_Sequence = []
+    words = loadWords(stories)
+    structor = [] 
+
+    print 'Loading stories...'
+    for word in words:
+        tri_Sequence.append(word.lower())
+        if len(tri_Sequence) == 3: #find new sequence
+            if not any(updateModel(elem,tri_Sequence) for elem in structor): structor.append(Tri_Gram(0, tri_Sequence))
+            tri_Sequence.remove(tri_Sequence[0])
+    print 'Stories loaded. The program is generating story...'   
+    generateStory(structor)
